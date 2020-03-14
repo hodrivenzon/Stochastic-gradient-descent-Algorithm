4c8199e0aee2b6b4a6c0c79e86c825c25df9abe8
hodrivenzon <hod.rivenzon@gmail.com>
2020-03-14 16:58:10 +0200

    Create main.py
    init struct

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+
+class SGD:
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+
+if __name__ == '__main__':
+    sgd = SGD()
+    sgd.fit()
+    print('Finish')
